367f13cc16328b3c163c4cbae1ad62bf771a4733
Philip Adelabu <adebisiphilip@gmail.com>
2025-10-07 19:37:46 +0100

    added the javascript code to evaluate the computing expression.

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -6,3 +6,67 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+
+const getChar = (e) => {
+	var input = document.getElementById('display-input');
+	var output = document.getElementById('display-result');
+	var i = e.innerHTML;
+	const inp = input.innerHTML;
+	var result = 0;
+
+
+   if(('/, *, -, +,.'.indexOf(i) > -1) && ('/, *, -, +, . '.indexOf(inp[inp.length-1]) > -1)){
+	    i = "";
+   }
+
+	if(i === '=' && input.innerHTML !== ''){
+		if(('/, *, -, + , ., ='.indexOf(i) > -1) && (input.innerHTML).length === 1){
+            input.innerHTML = "";
+			return ;
+		} 
+		 const exp = input.innerHTML;
+		 try{
+		 result = eval(exp);
+		 output.innerHTML = result;
+		 input.innerHTML = "";
+		 }catch(e){
+			output.innerHTML = "";
+		    input.innerHTML = "";
+		 }
+	
+		 return;
+	 }
+
+	 if(i === "CLR"){
+		 output.innerHTML = "";
+		 input.innerHTML = "";
+		 return ;
+	 }
+
+	 if(i === "STR"){
+		if(output.innerHTML !== ""){
+			result = output.innerHTML;
+			localStorage.setItem("mem", result);
+			output.innerHTML = "";
+		}
+		return;
+	 }
+
+	 if(i === "RCL"){
+		if(localStorage.getItem('mem') !== null){
+			output.innerHTML = localStorage.getItem("mem");
+		}
+		return;
+	 }
+
+	 if(output.innerHTML !== "" && inp.length == 0 && ('/, *, -, + '.indexOf(i) > -1)){
+	  input.innerHTML = output.innerHTML + "" + i;
+   } else
+     input.innerHTML = input.innerHTML + "" + i;
+	 
+
+	 
+}
+
